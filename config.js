ab35f07c3835c36f0d5a0d00aaff54b5790f720d
Rafay Essani <rafayessani@Rafays-MacBook-Pro.local>
2026-02-19 15:12:49 +0500

    Fix config.js semicolon

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "https://udywbynyqyoammvgnfev.supabase.co"
+window.SUPABASE_URL = "https://udywbynyqyoammvgnfev.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_IxIGUbPSzB7oFr5wf7XMsQ_NZaSKv-_";
